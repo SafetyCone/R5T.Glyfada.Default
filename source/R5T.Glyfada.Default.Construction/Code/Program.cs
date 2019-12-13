dad26db1b5b6629212d8baa3c927b99b6ca56a41
MinexAutomation <minex.automation@gmail.com>
2019-12-13 12:03:03 -0800

    Added test of git-init.

--- source/R5T.Glyfada.Default.Construction/Code/Program.cs
+++ source/R5T.Glyfada.Default.Construction/Code/Program.cs
@@ -1,12 +1,35 @@
 ﻿using System;
 
+using Microsoft.Extensions.DependencyInjection;
+
+using R5T.Richmond;
+using R5T.Derby;
+
+
 namespace R5T.Glyfada.Default.Construction
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Program.TryGitInitDirectory();
+        }
+
+        private static void TryGitInitDirectory()
+        {
+            var directoryPath = @"C:\Temp\Git\Test2";
+
+            var serviceProvider = Program.GetServiceProvider();
+
+            var gitOperator = serviceProvider.GetRequiredService<IGitOperator>();
+
+            gitOperator.Init(directoryPath);
+        }
+
+        private static IServiceProvider GetServiceProvider()
+        {
+            var serviceProvider = ApplicationBuilder.New().UseStartupWithDerbyConfigurationStartup<Startup>();
+            return serviceProvider;
         }
     }
 }
---
+++ source/R5T.Glyfada.Default.Construction/Code/Startup.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+
+using R5T.Caledonia;
+using R5T.Caledonia.Default;
+using R5T.Nikaia;
+using R5T.Nikaia.Configuration;
+using R5T.Nikaia.Configuration.Suebia;
+using R5T.Richmond;
+
+
+namespace R5T.Glyfada.Default.Construction
+{
+    public class Startup : ApplicationStartupBase
+    {
+        public Startup(ILogger<Startup> logger)
+            : base(logger)
+        {
+        }
+
+        protected override void ConfigureConfigurationBody(IConfigurationBuilder configurationBuilder, IServiceProvider configurationServicesProvider)
+        {
+            configurationBuilder
+                .AddGitConfigurationJsonFile(configurationServicesProvider)
+                ;
+        }
+
+        protected override void ConfigureServicesBody(IServiceCollection services)
+        {
+            services
+                .AddSingleton<IGitOperator, GitOperator>()
+                .AddSingleton<IGitOperatorCore, GitOperatorCore>()
+                .AddGitConfiguration()
+                .AddSingleton<IGitExecutableFilePathProvider, GitExecutableFilePathProvider>()
+                .AddSingleton<ICommandLineInvocationOperator, DefaultCommandLineInvocationOperator>()
+                ;
+        }
+    }
+}
