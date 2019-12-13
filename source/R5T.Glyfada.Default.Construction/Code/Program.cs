84b097dc080d7d63266780268d86a684b7d126f6
MinexAutomation <minex.automation@gmail.com>
2019-12-12 23:07:05 -0800

    Initial commit of source-stub.

---
+++ source/R5T.Glyfada.Default.Construction/Code/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace R5T.Glyfada.Default.Construction
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
