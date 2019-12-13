11cd4e64476d1a606d6fd501198272249d2a3d76
MinexAutomation <minex.automation@gmail.com>
2019-12-12 23:40:25 -0800

    Added static GitCommandLine.New() method building on CommandLine.New() to get a new ICommandBuilderContext implementation instance.

---
+++ source/R5T.Glyfada.Default/Code/Classes/GitCommandLine.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+using R5T.Heraklion;
+
+using R5T.Glyfada.Commands;
+using R5T.Volos;
+
+
+namespace R5T.Glyfada.Default
+{
+    public static class GitCommandLine
+    {
+        public static ICommandBuilderContext<GitContext> New()
+        {
+            var commandBuilderContext = CommandLine.New<GitContext>();
+            return commandBuilderContext;
+        }
+    }
+}
