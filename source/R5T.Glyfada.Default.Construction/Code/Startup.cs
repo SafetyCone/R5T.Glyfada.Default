5c407c474a91122d1550605c640a98f814d264f8
MinexAutomation <minex.automation@gmail.com>
2019-12-14 14:05:00 -0800

    Changed to use R5T.Glyfada.Standard (standard IGitOperator services aggregation) project-reference dependency.

--- source/R5T.Glyfada.Default.Construction/Code/Startup.cs
+++ source/R5T.Glyfada.Default.Construction/Code/Startup.cs
@@ -4,11 +4,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 
-using R5T.Caledonia;
-using R5T.Caledonia.Default;
-using R5T.Nikaia;
-using R5T.Nikaia.Configuration;
-using R5T.Nikaia.Configuration.Suebia;
+using R5T.Glyfada.Standard;
 using R5T.Richmond;
 
 
@@ -24,18 +20,14 @@ namespace R5T.Glyfada.Default.Construction
         protected override void ConfigureConfigurationBody(IConfigurationBuilder configurationBuilder, IServiceProvider configurationServicesProvider)
         {
             configurationBuilder
-                .AddGitConfigurationJsonFile(configurationServicesProvider)
+                .AddGitConfiguration(configurationServicesProvider)
                 ;
         }
 
         protected override void ConfigureServicesBody(IServiceCollection services)
         {
             services
-                .AddSingleton<IGitOperator, GitOperator>()
-                .AddSingleton<IGitOperatorCore, GitOperatorCore>()
-                .AddGitConfiguration()
-                .AddSingleton<IGitExecutableFilePathProvider, GitExecutableFilePathProvider>()
-                .AddSingleton<ICommandLineInvocationOperator, DefaultCommandLineInvocationOperator>()
+                .AddGitOperator()
                 ;
         }
     }
