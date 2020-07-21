5268526fa9a34872e07b298b40f462de7f04bea6
MinexAutomation <minex.automation@gmail.com>
2020-07-21 15:35:19 -0700

    Added IServiceCollection extensions.

---
+++ source/R5T.Glyfada.Default/Code/Extensions/IServiceCollectionExtensions.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+using Microsoft.Extensions.DependencyInjection;
+
+using R5T.Caledonia;
+using R5T.Dacia;
+using R5T.Nikaia;
+
+
+namespace R5T.Glyfada.Default
+{
+    public static class IServiceCollectionExtensions
+    {
+        /// <summary>
+        /// Adds the <see cref="GitOperator"/> implementation of <see cref="IGitOperator"/> as a <see cref="ServiceLifetime.Singleton"/>.
+        /// </summary>
+        public static IServiceCollection AddGitOperator(this IServiceCollection services,
+            IServiceAction<ICommandLineInvocationOperator> commandLineInvocationOperatorAction,
+            IServiceAction<IGitExecutableFilePathProvider> gitExecutableFilePathProviderAction)
+        {
+            services
+                .AddSingleton<IGitOperator, GitOperator>()
+                .Run(commandLineInvocationOperatorAction)
+                .Run(gitExecutableFilePathProviderAction)
+                ;
+
+            return services;
+        }
+
+        /// <summary>
+        /// Adds the <see cref="GitOperator"/> implementation of <see cref="IGitOperator"/> as a <see cref="ServiceLifetime.Singleton"/>.
+        /// </summary>
+        public static IServiceAction<IGitOperator> AddGitOperatorAction(this IServiceCollection services,
+            IServiceAction<ICommandLineInvocationOperator> commandLineInvocationOperatorAction,
+            IServiceAction<IGitExecutableFilePathProvider> gitExecutableFilePathProviderAction)
+        {
+            var serviceAction = ServiceAction.New<IGitOperator>(() => services.AddGitOperator(
+                commandLineInvocationOperatorAction,
+                gitExecutableFilePathProviderAction));
+
+            return serviceAction;
+        }
+    }
+}
